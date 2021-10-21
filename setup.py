88b48240c8e92b3a1ce3f416db34d70eefe2c803
Hamburger <72227790+VeryHamburger@users.noreply.github.com>
2021-10-21 23:27:24 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from animality import __version__
+from setuptools import setup
+
+setup(
+  name='animality-py',
+  packages=['animality'],
+  version=__version__,
+  license='MIT',
+  description='A python API wrapper for https://animality.xyz',
+  long_description=open('README.md', 'r', encoding='utf-8').read(),
+  long_description_content_type='text/markdown',
+  author='vierofernando',
+  entry_points={
+    'console_scripts': [
+      'animality = animality.cli'
+    ]
+  },
+  author_email='vierofernando9@gmail.com',
+  url='https://github.com/animality-xyz/animality-py',
+  download_url=f'https://github.com/animality-xyz/animality-py/archive/{__version__}.tar.gz',
+  keywords=['Animals', 'API', 'Hamburger', 'Pats', 'Headpats', 'API Wrapper'],
+  install_requires=[
+    'aiohttp',
+    'halo'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+  python_requires='>=3.7'
+)
