cf4139a942e4d7334aad7d749de22e703ef80cd4
Hamburger <72227790+VeryHamburger@users.noreply.github.com>
2022-05-12 23:18:23 +0100

    Update lib.py

--- animality/lib.py
+++ animality/lib.py
@@ -8,7 +8,7 @@ animals = [
     "cat", "dog", "bird", "panda",
     "redpanda", "koala", "fox", "whale",
     "kangaroo", "bunny", "lion", "bear",
-    "frog", "duck", "penguin"
+    "frog", "duck", "penguin", "axolotl"
 ]
 
 get_animal_name = compile('/img/([a-z]+)/').findall
